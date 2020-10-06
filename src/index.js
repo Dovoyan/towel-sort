f6404fa2d9f2fb90a5f32178c9dfd2ed4b07ecf4
Dovoyan <yankavalenkau@gmail.com>
2020-10-06 16:18:48 +0300

    Fear: did task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let z = 0;
+let arr = [];
+if (!Array.isArray(matrix)) return arr;
+    for (let i = 0; i < matrix.length; i++) {
+        if (i == 0 || i % 2 == 0) {
+            for (let y = 0; y < matrix[i].length; y++) {
+                arr[z] = matrix[i][y];
+                z++;
+            }
+        } else {
+            for (let y = matrix[i].length - 1; y > -1; y--) {
+                arr[z] = matrix[i][y];
+                z++;
+            }
+        }
+    }
+
+  
+
+  return arr;
 }
